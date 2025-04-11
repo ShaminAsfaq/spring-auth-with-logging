f02048c205b6dfed23e2129038571a9b3b6f50a7
shamin <shamin.asfaq@gmail.com>
2025-04-11 06:34:27 +0200

    WIP: - Need more robust Suppression of sensitive data - Next: Grafana Cloud. - Next: Ensure logs on production.

--- src/main/java/com/example/auth/controller/AuthController.java
+++ src/main/java/com/example/auth/controller/AuthController.java
@@ -5,9 +5,6 @@ import com.example.auth.model.dto.UserDTO;
 import com.example.auth.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@Slf4j
 public class AuthController {
     private final UserService userService;
-
-    private final Logger logger = LoggerFactory
-            .getLogger(AuthController.class);
 
     // Register endpoint
     @PostMapping("/register")
